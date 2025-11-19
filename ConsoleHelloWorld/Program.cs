bfc7ca7ab53d0311af4003f13fc061c1d407e6bc
9teeedev <teezeezax2@gmail.com>
2025-11-19 10:47:21 +0700

    updage

--- ConsoleHelloWorld/Program.cs
+++ ConsoleHelloWorld/Program.cs
@@ -4,14 +4,27 @@
     {
         static void Main(string[] args)
         {
+            string name = "";
+            string sex = "";
+            int height = 0;
+
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("My name is Phongsathorn Lansettha");
-            Console.WriteLine("Computer Science Program");
+            Console.WriteLine();
+            Console.Write("Please, enter your name (eng): ");
+            name = Console.ReadLine();
+            Console.Write("Please, enter your sex (M/F): ");
+            sex = Console.ReadLine();
+            Console.WriteLine("Welcome to C# Programming, " + name + "!");
+            Console.Write("Please, enter your height (cm): ");
+            height = Convert.ToInt16(Console.ReadLine());
+            int weight = 0;
+            if (sex == "M"){
+                weight = height - 100;
+            }else{
+                weight = height - 110;
+            }
+            Console.WriteLine("Your ideal weight is " + weight + " kg.");
             Console.WriteLine("----------------++++----------------");
-            Console.Write("My Hometown is ");
-            Console.Write("Udonthani ");
-            Console.Write("Thainalnd");
-            Console.ReadKey();
             {
                 
             }
