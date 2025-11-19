17543e98c6e09ed87dbe1a38275a68a4db06e130
9teeedev <teezeezax2@gmail.com>
2025-11-19 09:32:01 +0700

    Add ConsoleHelloWorld project

---
+++ ConsoleHelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Phongsathorn Lansettha");
+            Console.WriteLine("Computer Science Program");
+            Console.WriteLine("----------------++++----------------");
+            Console.Write("My Hometown is ");
+            Console.Write("Udonthani ");
+            Console.Write("Thainalnd");
+            Console.ReadKey();
+            {
+                
+            }
+        }
+    }
+}
